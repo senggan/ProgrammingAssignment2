ed5f6c09a57551afccace744a15782f14e3a9313
Seng Gan <senggan@gmail.com>
2015-02-15 19:10:09 -0500

    First Version of PA2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    # Initialize the solution to the inverse of matrix
+    
+    m <- NULL
+    
+    # We must reset m to NULL since we are modifying the underlying
+    # matrix and the cached value is no longer the valid.  
+    # Please note that if m is not set to null, 
+    # we cannot detect the matrx had changed
+    
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    # Please note that this works because x is stored in the function
+    get <- function(){
+        x
+    } 
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set=set, get=get,
+         setsolve=setsolve,
+         getsolve=getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## and the matrix has not changed, then the cachesolve should retrieve 
+## the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function test whether the passed argument is a list with 
+## "getsolve" and "setsolve" functions.
+## This is done to check and make sure that "makeCacheMatrix" is called 
+## before cacheSolve function.
+
+cacheSolve <- function(x=matrix(), ...) {
+    
+    ## Return a matrix that is the inverse of 'x'
+    if(class(x) =="list" ){
+        if ( class(x["getsolve"])== "function"){
+            if ( class(x["setsolve"])== "function"){
+                m <- x$getsolve()
+                if(!is.null(m)) {
+                    message("getting cached data")
+                    return(m)
+                }
+                else
+                {
+                    data <- x$get()
+                    m <- solve(data, ...)
+                    x$setsolve(m)
+                    m
+                }
+            }
+            else
+            {
+                message("You need to call makeCacheMatrix function first -> Failed setsolve")
+            }
+        }
+        else
+        {
+            message("You need to call makeCacheMatrix function first -> Failed getsolve")
+        }
+        
+    }
+    else
+    {
+        message("You need to call makeCacheMatrix function first-> Failed list")
+    }    
 }
